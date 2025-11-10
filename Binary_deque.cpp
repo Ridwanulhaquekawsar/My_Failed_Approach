3d47b1722569c43b24350e0ec9b9a6c2ef848ecf
Ridwanul Haque Kawsar <kawsarhaque430@gmail.com>
2025-11-10 09:10:16 +0600

    Update Binary_deque.cpp

--- Binary_deque.cpp
+++ Binary_deque.cpp
@@ -73,3 +73,51 @@ int main(){
 Note: 
   • My code is failed cause i choosed nearest possible element alternating the contigous element from end so this the wrong from my end cause if i choose nearest then the nerest all removal is extra operation if other end contain contigous element 🙂🧑‍💻
   • My code is not made best decision here, cause variable size sliding window has made best decision here 😎🧑‍💻
+
+
+
+Correct Approach:
+      • 👇
+
+#include<bits/stdc++.h>
+using namespace std;
+#define FastIO() ios::sync_with_stdio(0),cin.tie(0)
+using ll = long long int;
+#define nl '\n'
+
+ll t,n,s,sum,l,mx;
+vector<ll>a;
+
+void solve(){
+    cin>>n>>s;
+    a.resize(n);
+    
+    sum=0;
+    
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+        sum+=a[i];
+    }
+    
+    if(sum==s) cout<<0<<nl;
+    else if(sum<s) cout<<"-1"<<nl;
+    else{
+        l=0;
+        mx=-1;
+        sum=0;
+        
+        for(int r=0; r<n; r++){
+            sum+=a[r];
+            
+            while(sum>s) sum-=a[l++];
+            
+            if(sum==s) mx=max(mx,r-l+1);
+        }
+        
+        cout<<n-mx<<nl;
+    }
+}
+
+int main(){
+    FastIO(); cin>>t; while(t--) solve();
+}
