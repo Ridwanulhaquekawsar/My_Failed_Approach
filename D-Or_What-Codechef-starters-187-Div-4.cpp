3a0ddcf9c90f227d652dd1cdfc7d08f1c8684b28
Ridwanul Haque Kawsar <kawsarhaque430@gmail.com>
2026-02-01 14:41:41 +0600

    Create D-Or_What-Codechef-starters-187-Div-4.cpp

---
+++ D-Or_What-Codechef-starters-187-Div-4.cpp
@@ -0,0 +1,166 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FastIO() ios::sync_with_stdio(0),cin.tie(0)
+using ll = long long int;
+#define nl '\n'
+
+#define out(x) cout << (x) << nl
+#define in cin >>
+
+#define pinf LLONG_MAX
+#define ninf LLONG_MIN
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define Yes cout << "Yes\n"
+#define No cout << "No\n"
+
+#define f first
+#define sc second
+#define fr front()
+#define bc back()
+#define bg begin()
+#define l_b lower_bound
+#define u_b upper_bpund
+#define ed end()
+#define pu push
+#define po pop
+#define pf push_front
+#define pb push_back
+#define pob pop_back()
+#define pof pop_front()
+#define emp emplace
+#define clr clear()
+#define wh while
+#define emp_fr emplace_front
+#define emp_bc emplace_back
+#define mn_ele min_element
+#define sz(x) (ll)x.size()
+
+#define rs(v, n) (v).resize(n)
+#define as(v, sz, val) (v).assign(sz, val)
+
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define vvll vector< vector<ll> >
+#define vpll vector<pll>
+#define S string
+
+#define St set<ll>
+#define Ms multiset<ll>
+#define Mp map<ll, ll>
+#define Ump unordered_map<ll, ll>
+#define Q queue<ll>
+#define Pq priority_queue<ll>
+#define Dq deque<ll>
+
+#define rep(i, n) for(ll i = 0; i < n; i++)
+#define rrep(i, n) for(ll i = n - 1; i >= 0; i--)
+#define rep1(i, a, b) for(ll i = a; i <= b; i++)
+#define rrep1(i, b, a) for(ll i = b; i >= a; i--)
+
+bool isPrime(ll n){
+
+    if(n == 1) return 0;
+    else if(n == 2) return 1;
+    else if(n % 2 == 0) return 0;
+    
+    for(ll i = 3; i <= n/i; i += 2){
+        if(n % i == 0) return 0;
+    }
+    
+    return 1;
+}
+
+class cmp{
+    public:
+    bool operator()(){
+        
+    }
+};
+
+/*
+ll lcm(ll a, ll b) {
+    return (a / __gcd(a, b)) * b;
+}
+*/
+
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+*/
+
+ll T;
+ll n, x;
+ll fri;
+ll low, high;
+vll ans;
+vll tmp;
+ll j;
+
+void solve(){
+    in n >> x;
+    
+    fri = __builtin_popcount(x);
+    
+    if(fri > 2){
+        rep(i, n) cout << i + 1 << " ";
+        cout << nl;
+    }
+    else if(fri == 1){
+        rep(i, n) cout << i + 1 << " ";
+        cout << nl;
+    }    
+    else{
+        low = x & -x;
+        high = x ^ low;
+        
+        // low = (1ll << (low - 1) );
+        // hight = (1ll << (hight - 1) );
+        
+        if(high > n){
+            rep(i, n) cout << i + 1 << " ";
+            cout << nl;
+        }
+        else{
+            as(ans, n + 1, 0);
+            tmp.clr;
+            
+            rep1(i, 1, n) if(i != low && i != high) tmp.pb(i);
+            
+            ans[low] = high;
+            ans[high] = low;
+            
+            j = 1;
+            
+            for(auto &x : tmp){
+                if(ans[j] == 0) ans[j] = x;
+                else{
+                    while(ans[j] != 0) j++;
+                    
+                    ans[j] = x;
+                }
+            }
+            
+            rep1(i, 1, n) cout << ans[i] << " ";
+            cout << nl;
+        }
+    } 
+    
+    // cout << low << high << "  ok";
+}
+
+int main(){
+    FastIO(); 
+    
+    T = 1;
+    in T;
+    while(T--) solve();
+}
