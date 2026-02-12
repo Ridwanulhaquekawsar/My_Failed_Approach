b3ad37e7d656383c9db5cce7b36177125f4f874a
Ridwanul Haque Kawsar <kawsarhaque430@gmail.com>
2026-02-12 20:49:42 +0600

    Create C-Temporary_Temperature-codechef-starters-161-Div-4.cpp

---
+++ C-Temporary_Temperature-codechef-starters-161-Div-4.cpp
@@ -0,0 +1,185 @@
+C++ Code : 👇
+  
+
+#include<bits/stdc++.h>
+using namespace std;
+#define FastIO() ios::sync_with_stdio(0),cin.tie(0)
+using ll = long long int;
+#define nl '\n'
+
+#define out(x) cout << (x) << nl
+#define in cin >>
+
+#define pinf LLONG_MAX
+#define ninf LLONG_MIN
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+
+#define Mx_ele(x) *max_element(all(x) )
+#define Mn_ele(x) *min_element(all(x) )
+#define Sum(x) accumulate(all(x), 0LL)
+#define Cnt(x,val) count(all(x), val)
+
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define Yes cout << "Yes\n"
+#define No cout << "No\n"
+
+#define f first
+#define sc second
+#define fr front()
+#define bc back()
+#define bg begin()
+#define rbg rbegin()
+#define l_b lower_bound
+#define u_b upper_bound
+#define ed end()
+#define pu push
+#define po pop
+#define pf push_front
+#define pb push_back
+#define pob pop_back()
+#define pof pop_front()
+#define emp emplace
+#define clr clear()
+#define wh while
+#define emp_fr emplace_front
+#define emp_bc emplace_back
+#define mn_ele min_element
+#define mx_ele max_element
+#define sz(x) (ll)x.size()
+#define b_p __builtin_popcount
+
+#define rs(v, n) (v).resize(n)
+#define as(v, sz, val) (v).assign(sz, val)
+
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define vvll vector< vector<ll> >
+#define vpll vector<pll>
+#define S string
+
+#define St set<ll>
+#define Ml multiset<ll>
+#define Mlpll multiset<pll>
+#define Mp map<ll, ll>
+#define Ump unordered_map<ll, ll>
+#define Q queue<ll>
+#define Pq priority_queue<ll>
+#define Dq deque<ll>
+
+#define rep(i, n) for(ll i = 0; i < n; i++)
+#define rrep(i, n) for(ll i = n - 1; i >= 0; i--)
+#define rep1(i, a, b) for(ll i = a; i <= b; i++)
+#define rrep1(i, b, a) for(ll i = b; i >= a; i--)
+
+vll getDivisors(ll n){
+    vll divisors;
+    
+    for(ll i = 1; i <= n/i; i++){
+    
+        if(n % i == 0){
+            divisors.pb(i);
+            if(i != n/i) divisors.pb(n/i);
+        }
+    }
+    
+    sort(all(divisors) );
+    return divisors;
+}
+
+bool isPrime(ll n){
+
+    if(n == 1) return 0;
+    else if(n == 2) return 1;
+    else if(n % 2 == 0) return 0;
+    
+    for(ll i = 3; i <= n/i; i += 2){
+        if(n % i == 0) return 0;
+    }
+    
+    return 1;
+}
+
+class cmp{
+    public:
+    bool operator()(){
+        
+    }
+};
+
+/*
+ll lcm(ll a, ll b) {
+    return (a / __gcd(a, b)) * b;
+}
+*/
+
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+*/
+
+ll T;
+ll n;
+vll v;
+vector< tuple<ll, ll, ll> >ans;
+ll mn, mx;
+ll diff;
+ll f, s;
+ll k;
+
+void solve(){
+    in n >> k;
+    rs(v, n);
+    ans.clr;
+    
+    mn = pinf;
+    mx = ninf;
+    
+    rep(i, n){
+        in v[i];
+        
+        mn = min(mn, v[i]);
+        mx = max(mx, v[i]);
+    }
+    
+    if(k >= n - 1){
+        out(0);
+        return;
+    }    
+    
+    diff = (mx - mn);
+    
+    rep(i, n - 1) ans.pb({abs(v[i] - v[i + 1]), v[i], v[i + 1]});
+    sort(rall(ans) );
+    
+    if(k == 0) (diff & 1) ? out(diff/2 + 1) : out(diff/2);
+    else{
+        mn = pinf;
+        mx = ninf;
+        
+        rep1(i, k, sz(ans) - 1){
+            f = get<1>(ans[i]);
+            s = get<2>(ans[i]);
+            
+            mn = min({mn, f, s});
+            mx = max({mx, f, s});
+        }
+        
+        diff = (mx - mn);
+        
+        (diff & 1) ? out(diff/2 + 1) : out(diff/2); 
+    }
+}
+    
+int main(){
+    FastIO(); 
+    
+    T = 1;
+    in T;
+    while(T--) sol
